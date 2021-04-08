5a3c00476386a4daf8bf8bad081d2e6e9a22da36
Michal Kaštan <michal.kastan99@gmail.com>
2021-04-08 22:02:50 +0200

    ✨ Added Cypress testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,11 +31,12 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:cypress/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2017,
         sourceType: 'module',
     },
-    plugins: ['prettier', '@typescript-eslint'],
+    plugins: ['prettier', '@typescript-eslint', 'cypress'],
 };
---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+
+describe('Test', () => {
+    it('Not much happening here', () => {
+        expect(true).to.equal(true);
+    });
+});
