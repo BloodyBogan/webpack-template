7bed5e22bdc4271aacfe97c47790433b917b19cf
Michal Kaštan <michal.kastan99@gmail.com>
2021-04-08 22:10:52 +0200

    ✨ Added sample fixture

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -5,3 +5,15 @@ describe('Test', () => {
         expect(true).to.equal(true);
     });
 });
+
+describe('Fixture test', () => {
+    let sampleData;
+
+    before(async () => {
+        sampleData = await cy.fixture('sample');
+    });
+
+    it('Is running', () => {
+        expect(sampleData.running).to.equal(true);
+    });
+});
